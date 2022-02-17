3bcacab92f6034b999f45ba61ecf26e7a77632e4
Thiago Pereira <thiagopereiramail@gmail.com>
2022-02-17 19:51:33 +0000

    ativa clique nos botões

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function watch() {
+  window.open(
+    "https://www.youtube.com/embed/tgbNymZ7vqY",
+    "_blank",
+    "top=200,left=400,width=500,height=300"
+  );
+}
+
+function moreInfo() {
+  window.open(
+    "https://www.themoviedb.org/tv/1425-house-of-cards?language=pt-BR"
+  );
+}
